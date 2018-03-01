bedca5c87454e7a10790f0c91f98f9d20d512a2e
ylj <564296557@qq.com>
2018-03-01 15:05:15 +0800

    add analysis

--- ALGORITHMS/INSERTION_SORT.js
+++ ALGORITHMS/INSERTION_SORT.js
@@ -21,4 +21,35 @@ function insertionSort(arr) {                  // cost   times
     return arr;
 }
 
+/*
+                                        n       n             n
+ T(n) = c1n + c2(n - 1) + c4(n - 1) + c5∑tj + c6∑(tj - 1) + c7∑(tj - 1) + c8(n - 1)
+                                       j=2     j=2           j=2
+
+the best case occurs if the array is already sorted. For each j = 2,3,...,n, we thenfind that A[i] <=  key in line 5 when i has its initial value of j - 1. Thus tj = 1 for j = 2,3,...,n, and the best-case running time is
+
+T(n) = c1n + c2(n - 1) + c4(n -1) + c5(n - 1) + c8(n -1)
+     = (c1 + c2 + c4 + c5 + c8)n - (c2 + c4 + c5 + c8)
+     = an + b  // linear function
+
+
+If the array is in reverse sorted order --- that is, in decreasing order --- the worst case results. We must compare each element A[j] with each element in the entire sorted subarray A[1..j - 1], and so tj = j for j = 2,3,...,n.
+ 
+
+ n
+ ∑j = n(n + 1) / 2 - 1
+j=2
+
+and
+
+ n
+ ∑j = n(n - 1) / 2
+j=2
+
+
+T(n) = c1n + c2(n - 1) + c4(n - 1) + c5(n(n + 1) / 2 - 1) + c6(n(n - 1) / 2) + c7(n(n - 1) / 2) + c8(n - 1)
+     = (c5/2 + c6/2 + c7/2)n^2 + (c1 + c2 + c4 + c5/2 - c6/2 - c7/2 + c8)n - (c2 + c4 + c5 + c8)
+     = an^2 + bn + c   // quadratic function
+ * /
+
 module.exports = insertionSort;
