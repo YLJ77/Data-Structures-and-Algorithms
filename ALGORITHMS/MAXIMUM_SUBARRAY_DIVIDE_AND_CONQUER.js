39ab000f75cec33fd3a53b69e5ab5e47e38efdf9
ylj <564296557@qq.com>
2018-03-06 14:54:27 +0800

    add T(n)

--- ALGORITHMS/MAXIMUM_SUBARRAY_DIVIDE_AND_CONQUER.js
+++ ALGORITHMS/MAXIMUM_SUBARRAY_DIVIDE_AND_CONQUER.js
@@ -4,19 +4,19 @@ function findMaxCrossingSubArray(array, left, middle, right) {
     let sum = 0;
 
     for (let i=middle; i>=left; i-=1) {
-        if (sum + array[i] >= leftSum) {
-            leftSum = sum + array[i];
-        }
         sum += array[i];
+        if (sum >= leftSum) {
+            leftSum = sum;
+        }
     }
 
     sum = 0;
 
-    for (let i=middle + 1; i<right; i+=1) {
-        if (sum + array[i] >= rightSum) {
-            rightSum = sum + array[i];
-        }
+    for (let i=middle + 1; i<=right; i+=1) {
         sum += array[i];
+        if (sum >= rightSum) {
+            rightSum = sum;
+        }
     }
 
     return leftSum + rightSum;
@@ -29,11 +29,17 @@ function findMaximumSubArray(array, left = 0, right = array.length) {
     }
 
     let middle = Math.floor((left + right) / 2);
-    let leftSum = findMaximumSubArray(array, left, middle);
-    let rightSum = findMaximumSubArray(array, middle, right);
-    let crossSum = findMaxCrossingSubArray(array, left, middle, right);
+    let leftSum = findMaximumSubArray(array, left, middle);    // T(n/2)
+    let rightSum = findMaximumSubArray(array, middle, right);  // T(n/2)
+    let crossSum = findMaxCrossingSubArray(array, left, middle, right);  // Θ(n)
 
-    return Math.max(crossSum, leftSum, rightSum);
+    return Math.max(crossSum, leftSum, rightSum);  // Θ(1)
 }
+
+/*
+ 
+T(n) = { Θ(1)             if n = 1
+         2T(n/2) + Θ(n)   if n > 1
+ * */
 
 module.exports = findMaximumSubArray;
